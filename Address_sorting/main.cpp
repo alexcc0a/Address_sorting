e4d30dcc67ddb1dcae5f29ea2426b19926ef2b85
Alexander Nesterov <alex18nest@icloud.com>
2024-07-30 19:01:42 -0300

    [+] Correction sort

--- Address_sorting/main.cpp
+++ Address_sorting/main.cpp
@@ -11,36 +11,49 @@
 
 class Address {
 public:
-    void new_city(std::string city) {
-        this->city = city;
-    }
-    std::string get_city() {
-        return city;
-    }
-    void init_ad(std::string city, std::string street, int home_number, int apartment_number) {
+    Address() {}
+    Address(std::string city, std::string street, int house_number, int apartment_number) {
         this->city = city;
         this->street = street;
-        this->home_number = home_number;
+        this->house_number = house_number;
         this->apartment_number = apartment_number;
     }
-    std::string ad() {
-        return city + ", " + street + ", " + std::to_string(home_number) + ", " + std::to_string(apartment_number);
+
+    Address(const Address& other) {
+        city = other.city;
+        street = other.street;
+        house_number = other.house_number;
+        apartment_number = other.apartment_number;
     }
+
+    std::string get_city() const {
+        return city;
+    }
+
+    void init_ad(std::string city, std::string street, int house_number, int apartment_number) {
+        this->city = city;
+        this->street = street;
+        this->house_number = house_number;
+        this->apartment_number = apartment_number;
+    }
+    std::string ad() const {
+        return city + ", " + street + ", " + std::to_string(house_number) + ", " + std::to_string(apartment_number);
+    }
+
 private:
     std::string city;
     std::string street;
-    int home_number;
+    int house_number;
     int apartment_number;
 };
 
 void sort(Address* ad, int num) {
     for (int i = 0; i < num; i++) {
-        for (int j = 0; j < num -1; j++) {
+        for (int j = 0; j < num - 1; j++) {
             if (ad[j + 1].get_city() < ad[j].get_city()) {
-                std::string str;
-                str = ad[j].get_city();
-                ad[j].new_city(ad[j + 1].get_city());
-                ad[j + 1].new_city(str);
+                Address temp(ad[j]);
+                ad[j] = Address(ad[j + 1]);
+                ad[j + 1] = temp;
             } else {
                 continue;
             }
@@ -50,37 +63,34 @@ void sort(Address* ad, int num) {
 
 int main() {
 
-  std::ifstream file("in.txt");
-  std::ofstream File("out.txt");
+    std::ifstream file("in.txt");
+    std::ofstream File("out.txt");
 
-  setlocale(LC_ALL, "Russian");
-  int num;
-  file >> num;
-  File << num << std::endl;
-  Address address;
-  Address* ad = new Address[num];
+    setlocale(LC_ALL, "Russian");
+    int num;
+    file >> num;
+    File << num << std::endl;
 
-  std::string city;
-  std::string street;
-  int home_number;
-  int apartment_number;
+    Address* ad = new Address[num];
 
-  for (int j = 0; j < num; j++) {
-    file >> city;
-    file >> street;
-    file >> home_number;
-    file >> apartment_number;
-    ad[j].init_ad(city, street, home_number, apartment_number);
+    std::string city;
+    std::string street;
+    int house_number;
+    int apartment_number;
 
-  }
+    for (int j = 0; j < num; j++) {
+        file >> city;
+        file >> street;
+        file >> house_number;
+        file >> apartment_number;
+        ad[j].init_ad(city, street, house_number, apartment_number);
+    }
 
-  sort(ad, num);
+    sort(ad, num);
 
-  for (int j = 0; j < num; j++) {
+    for (int j = 0; j < num; j++) {
+        File << ad[j].ad() << std::endl;
+    }
 
-    File << ad[j].ad() << std::endl;
-
-  }
-
-  delete[] ad;
+    delete[] ad;
 }
